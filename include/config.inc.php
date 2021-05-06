48fb0d36c79d8032d9e2beb31fc31aea5fa2ddc2
Giulio Criscoli <45789083+gcriscoli@users.noreply.github.com>
2021-05-06 11:37:34 +0200

    Add files via upload

---
+++ include/config.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$settings = array(
+				  'DBParams' => array(
+								'hostname'	=>	'MySQL80',
+								'username'	=>	'root',
+								'password'	=>	'P455word!',
+								'database'	=>	'piggybank'
+								)
+				  );
+?>
